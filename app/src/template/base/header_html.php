e0209108d2bfc584a7ee50782b72549338a3aa66
Justine Martin <justine.martin.dev@gmail.com>
2020-11-26 13:06:21 +0100

    css : CSS Basique

--- app/src/template/base/header_html.php
+++ app/src/template/base/header_html.php
@@ -2,6 +2,8 @@
 <html>
     <head>
         <title><?= empty($title) ? WEBSITE_DEFAULT_NAME : $title . " - " . WEBSITE_DEFAULT_NAME?></title>
+        <link rel="stylesheet" href="css/style.css">
     </head>
 
     <body>
+        <main class="container">
