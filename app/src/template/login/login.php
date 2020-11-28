076eabac82986eb44b173c276290f2365e012977
Justine Martin <justine.martin.dev@gmail.com>
2020-11-28 16:16:15 +0100

    ui : Design de la page de login

--- app/src/template/login/login.php
+++ app/src/template/login/login.php
@@ -4,13 +4,15 @@
 
 <?= $errorDiv?>
 
-<form action="<?= $urlAction?>" method="POST">
-    <label for="<?= BuilderUtilisateur::FIELD_LOGIN?>">Login</label><input name="<?= BuilderUtilisateur::FIELD_LOGIN?>" placeholder="Login" id="<?= BuilderUtilisateur::FIELD_LOGIN?>" value="<?= $loginFieldValue?>"/>
-    <label for="<?= BuilderUtilisateur::FIELD_PASSWORD?>">Mot de passe</label><input name="<?= BuilderUtilisateur::FIELD_PASSWORD?>" placeholder="Mot de passe" id="<?= BuilderUtilisateur::FIELD_PASSWORD?>" type="password"/>
-   
-    <input type="submit" value="Valider"/>
-</form>
+<form class="form-center" action="<?= $urlAction?>" method="POST">
+    <h2>Connexion</h2>
+    <label for="<?= BuilderUtilisateur::FIELD_LOGIN?>">Login</label>
+    <input name="<?= BuilderUtilisateur::FIELD_LOGIN?>" placeholder="Login" id="<?= BuilderUtilisateur::FIELD_LOGIN?>" value="<?= $loginFieldValue?>"/>
 
+    <label for="<?= BuilderUtilisateur::FIELD_PASSWORD?>">Mot de passe</label>
+    <input name="<?= BuilderUtilisateur::FIELD_PASSWORD?>" placeholder="Mot de passe" id="<?= BuilderUtilisateur::FIELD_PASSWORD?>" type="password"/>
+    <input type="submit" value="Connexion"/>
+</form>
 <?php
     include("template/base/footer_html.php");
 ?>
