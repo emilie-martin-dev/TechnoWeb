c0e826d49d5d33c9412c8a8eb5ad6658be820523
Justine Martin <justine.martin.dev@gmail.com>
2020-12-03 15:52:11 +0100

    ui : Design de la page d'inscription

--- app/src/template/sign_up/sign_up.php
+++ app/src/template/sign_up/sign_up.php
@@ -4,11 +4,19 @@
 
 <?= $errorDiv?>
 
-<form action="<?= $urlAction?>" method="POST">
-    <label for="<?= BuilderUtilisateur::FIELD_NOM?>">Nom</label><input name="<?= BuilderUtilisateur::FIELD_NOM?>" placeholder="Nom" id="<?= BuilderUtilisateur::FIELD_NOM?>"/>
-    <label for="<?= BuilderUtilisateur::FIELD_PRENOM?>">Prénom</label><input name="<?= BuilderUtilisateur::FIELD_PRENOM?>" placeholder="Prénom" id="<?= BuilderUtilisateur::FIELD_PRENOM?>"/>
-    <label for="<?= BuilderUtilisateur::FIELD_LOGIN?>">Login</label><input name="<?= BuilderUtilisateur::FIELD_LOGIN?>" placeholder="Login" id="<?= BuilderUtilisateur::FIELD_LOGIN?>"/>
-    <label for="<?= BuilderUtilisateur::FIELD_PASSWORD?>">Mot de passe</label><input name="<?= BuilderUtilisateur::FIELD_PASSWORD?>" placeholder="Mot de passe" id="<?= BuilderUtilisateur::FIELD_PASSWORD?>" type="password"/>
+<form class="form-center w6" action="<?= $urlAction?>" method="POST">
+    <h2>Inscription</h2>
+    <label for="<?= BuilderUtilisateur::FIELD_NOM?>">Nom</label>
+    <input name="<?= BuilderUtilisateur::FIELD_NOM?>" placeholder="Nom" id="<?= BuilderUtilisateur::FIELD_NOM?>"/>
+    
+    <label for="<?= BuilderUtilisateur::FIELD_PRENOM?>">Prénom</label>
+    <input name="<?= BuilderUtilisateur::FIELD_PRENOM?>" placeholder="Prénom" id="<?= BuilderUtilisateur::FIELD_PRENOM?>"/>
+    
+    <label for="<?= BuilderUtilisateur::FIELD_LOGIN?>">Login</label>
+    <input name="<?= BuilderUtilisateur::FIELD_LOGIN?>" placeholder="Login" id="<?= BuilderUtilisateur::FIELD_LOGIN?>"/>
+    
+    <label for="<?= BuilderUtilisateur::FIELD_PASSWORD?>">Mot de passe</label>
+    <input name="<?= BuilderUtilisateur::FIELD_PASSWORD?>" placeholder="Mot de passe" id="<?= BuilderUtilisateur::FIELD_PASSWORD?>" type="password"/>
     
     <input type="submit" value="Valider"/>
 </form>
