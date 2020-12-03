01293527e7c3527d55d5c1a5ab788d0ad33156e5
Justine Martin <justine.martin.dev@gmail.com>
2020-12-03 15:54:34 +0100

    ui : Design page config admin

--- app/src/template/config/config.php
+++ app/src/template/config/config.php
@@ -2,14 +2,15 @@
     include("template/base/header_html.php");
 ?>
 
-<h1><?= $title?></h1>
 
 <?= $errorDiv?>
 
-<form action="<?= $urlAction?>" method="POST">
-    <label for="<?= BuilderConfig::FIELD_LIBELLE?>">Valeur du <?=$libelleFieldValue?> </label>
+<form class="form-center w12" action="<?= $urlAction?>" method="POST">
+    
+    <h2><?= $title?></h2>
+    <label for="<?= BuilderConfig::FIELD_LIBELLE?>"><?=$libelleFieldValue?> (secondes)</label>
     <input name="<?= BuilderConfig::FIELD_VALEUR?>" placeholder="Valeur" id="<?= BuilderConfig::FIELD_VALEUR?>" value="<?= $valeurFieldValue?>"/>
-    <label>secondes</label>  
+    
     <input type="submit" value="Valider"/>
 </form>
 
