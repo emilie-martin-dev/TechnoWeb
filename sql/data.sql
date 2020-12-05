984968e86c70d9be8ce3f67f830f837e0c18bb98
Justine Martin <justine.martin.dev@gmail.com>
2020-12-05 11:05:23 +0100

    sql : Ajout du dump de la BDD et complète le script de data

--- sql/data.sql
+++ sql/data.sql
@@ -1,42 +1,47 @@
-INSERT INTO CONFIG VALUES(NULL, "COOKIE_LOGIN_TIMEOUT", "2592000");
+INSERT INTO CONFIG VALUES(NULL, 'COOKIE_LOGIN_TIMEOUT', '2592000');
 
-INSERT INTO ROLE VALUES(NULL, "ADMIN");
-INSERT INTO ROLE VALUES(NULL, "USER");
+INSERT INTO ROLE VALUES(NULL, 'ADMIN');
+INSERT INTO ROLE VALUES(NULL, 'USER');
 
-INSERT INTO UTILISATEUR VALUES(NULL, "Vanier", "Pascal", "vanier", "$2y$10$NaEOQXtS6wZqm.zblKtO0OLZlro59zQ/MxnU5BsccE2.NCNJCSi26", 2);
-INSERT INTO UTILISATEUR VALUES(NULL, "Lecarpentier", "Jean-Marc", "lecarpentier", "$2y$10$NaEOQXtS6wZqm.zblKtO0OLZlro59zQ/MxnU5BsccE2.NCNJCSi26", 2);
-INSERT INTO UTILISATEUR VALUES(NULL, "admin", "root", "admin", "$2y$10$NaEOQXtS6wZqm.zblKtO0OLZlro59zQ/MxnU5BsccE2.NCNJCSi26", 1);
+INSERT INTO UTILISATEUR VALUES(NULL, 'Vanier', 'Pascal', 'vanier', '$2y$10$NaEOQXtS6wZqm.zblKtO0OLZlro59zQ/MxnU5BsccE2.NCNJCSi26', 2);
+INSERT INTO UTILISATEUR VALUES(NULL, 'Lecarpentier', 'Jean-Marc', 'lecarpentier', '$2y$10$NaEOQXtS6wZqm.zblKtO0OLZlro59zQ/MxnU5BsccE2.NCNJCSi26', 2);
+INSERT INTO UTILISATEUR VALUES(NULL, 'admin', 'root', 'admin', '$2y$10$NaEOQXtS6wZqm.zblKtO0OLZlro59zQ/MxnU5BsccE2.NCNJCSi26', 1);
+INSERT INTO UTILISATEUR VALUES(NULL,'Dupont','Jean','j.dupont@mail.org','$2y$10$RTXzAESHlFiUaeBTrHhq..WReEfay9FBNFi22ZInzzUfRyIp9Sm4G',2)
+INSERT INTO UTILISATEUR VALUES(NULL,'ElMoataz','Abderrahim','elmoataz.a@greyc.org','$2y$10$bd0F7/R1LE/z6kRuJITlM.8hK6mUDeU.e5/cEYhWEieoYNRhQzVMi',2);
 
-INSERT INTO ACTIVITE
-VALUES(NULL, 'Mont Saint Michel', 'Mont Saint Michel', 
-    "Le Mont-Saint-Michel est une commune française située dans le département de la Manche en Normandie. Elle tire son nom de l'îlot rocheux consacré à saint Michel où s'élève aujourd'hui l'abbaye du
-Mont-Saint-Michel.", 
-    "Le Mont-Saint-Michel est une commune française située dans le département de la Manche en Normandie. Elle tire son nom de l'îlot rocheux consacré à saint Michel où s’élève aujourd’hui l’abbaye du Mont-Saint-Michel.
-    \n\nL’architecture du Mont-Saint-Michel et sa baie en font le site touristique le plus fréquenté de Normandie et l'un des dix plus fréquentés en France— premier site après ceux d'Île-de-France — avec près de deux 
-    millions et demi de visiteurs chaque année (3 250 000 en 2006, 2 300 000 en 2014).\n\nUne statue de saint Michel placée au sommet de l’église abbatiale culmine à 157,10 mètres au-dessus du rivage. 
-    Élément majeur, l'abbaye et ses dépendances sont classées au titre des monuments historiques par la liste de 1862 (60 autres constructions étant protégées par la suite) ; l'îlot et le cordon littoral de la baie 
-    figurent depuis 1979 sur la liste du patrimoine mondial de l’UNESCO ainsi que le moulin de Moidrey depuis 2007. Par ailleurs le mont bénéficie d'une seconde reconnaissance mondiale en tant qu'étape des Chemins de 
-    Saint-Jacques-de-Compostelle en France pour « les pèlerins du Nord de l'Europe (qui) passaient par le Mont lorsqu'ils se rendaient en Galice ». \n\nEn 2017, la commune comptait 30 habitants, appelés les Montois. 
-    L'îlot du mont Saint-Michel est devenu au fil du temps un élément emblématique du patrimoine français.", 1);
+INSERT INTO ACTIVITE VALUES(1,'Mont Saint Michel','Mont Saint Michel','Le Mont-Saint-Michel est une commune française située dans le département de la Manche en Normandie. Elle tire son nom de l\'îlot rocheux consacré à saint Michel où s\'élève aujourd\'hui l\'abbaye du\nMont-Saint-Michel.','Le Mont-Saint-Michel est une commune française située dans le département de la Manche en Normandie. Elle tire son nom de l\'îlot rocheux consacré à saint Michel où s’élève aujourd’hui l’abbaye du Mont-Saint-Michel.\n    \n\nL’architecture du Mont-Saint-Michel et sa baie en font le site touristique le plus fréquenté de Normandie et l\'un des dix plus fréquentés en France— premier site après ceux d\'Île-de-France — avec près de deux \n    millions et demi de visiteurs chaque année (3 250 000 en 2006, 2 300 000 en 2014).\n\nUne statue de saint Michel placée au sommet de l’église abbatiale culmine à 157,10 mètres au-dessus du rivage. \n    Élément majeur, l\'abbaye et ses dépendances sont classées au titre des monuments historiques par la liste de 1862 (60 autres constructions étant protégées par la suite) ; l\'îlot et le cordon littoral de la baie \n    figurent depuis 1979 sur la liste du patrimoine mondial de l’UNESCO ainsi que le moulin de Moidrey depuis 2007. Par ailleurs le mont bénéficie d\'une seconde reconnaissance mondiale en tant qu\'étape des Chemins de \n    Saint-Jacques-de-Compostelle en France pour « les pèlerins du Nord de l\'Europe (qui) passaient par le Mont lorsqu\'ils se rendaient en Galice ». \n\nEn 2017, la commune comptait 30 habitants, appelés les Montois. \n    L\'îlot du mont Saint-Michel est devenu au fil du temps un élément emblématique du patrimoine français.',1);
 
-INSERT INTO ACTIVITE
-VALUES(NULL, 'Le Scriptorial', 'Avranches', 
-    "Le Scriptorial est le musée des manuscrits du Mont Saint-Michel, installé à Avranches (Manche).", 
-    "Ouvert en août 2006, le Scriptorial est situé à l'abri des remparts de la ville d'Avranches. Son objectif est de valoriser et faire découvrir l'exceptionnelle collection des manuscrits de l'ancienne abbaye du Mont-Saint-Michel. 
-    L'architecture du bâtiment s'inspire de celle du Mont-Saint-Michel ; les rampes douces menant à la salle du trésor rappellent celles du célèbre Mont.\n\nLe musée replace les manuscrits médiévaux dans leur contexte historique 
-    et local grâce à un parcours chronologique et thématique qui mène à la découverte des œuvres originales. Il permet la découverte des différentes phases de réalisation des manuscrits, la calligraphie, l'enluminure ; il aborde 
-    le contenu des textes anciens, puis évoque l'évolution vers l'imprimerie et la bibliothèque du fonds ancien et de ses 14 000 ouvrages.\n\n Le Scriptorial est un musée moderne dédié à l'écriture et aux livres, dessiné par 
-    les architectes Emmanuel Berjot, Daniel Cléris et Jean-Michel Daubourg, la scénographie est de Nathalie Chauvier et Nicolas Béquart de l'agence les crayons. Grâce aux outils informatiques, la visite est interactive : bornes 
-    multimédia, livres électroniques, projections vidéo. Il propose également des expositions temporaires, des ateliers pédagogiques ainsi que des cycles de conférences. ", 1);
+INSERT INTO ACTIVITE VALUES(2,'Le Scriptorial','Avranches','Le Scriptorial est le musée des manuscrits du Mont Saint-Michel, installé à Avranches (Manche).','Ouvert en août 2006, le Scriptorial est situé à l\'abri des remparts de la ville d\'Avranches. Son objectif est de valoriser et faire découvrir l\'exceptionnelle collection des manuscrits de l\'ancienne abbaye du Mont-Saint-Michel. \n    L\'architecture du bâtiment s\'inspire de celle du Mont-Saint-Michel ; les rampes douces menant à la salle du trésor rappellent celles du célèbre Mont.\n\nLe musée replace les manuscrits médiévaux dans leur contexte historique \n    et local grâce à un parcours chronologique et thématique qui mène à la découverte des œuvres originales. Il permet la découverte des différentes phases de réalisation des manuscrits, la calligraphie, l\'enluminure ; il aborde \n    le contenu des textes anciens, puis évoque l\'évolution vers l\'imprimerie et la bibliothèque du fonds ancien et de ses 14 000 ouvrages.\n\n Le Scriptorial est un musée moderne dédié à l\'écriture et aux livres, dessiné par \n    les architectes Emmanuel Berjot, Daniel Cléris et Jean-Michel Daubourg, la scénographie est de Nathalie Chauvier et Nicolas Béquart de l\'agence les crayons. Grâce aux outils informatiques, la visite est interactive : bornes \n    multimédia, livres électroniques, projections vidéo. Il propose également des expositions temporaires, des ateliers pédagogiques ainsi que des cycles de conférences. ',1);
 
-INSERT INTO ACTIVITE
-VALUES(NULL, 'Château de Gratot', 'Gratot', 
-    "Le château de Gratot est à l'origine une maison forte, dont les vestiges subsistants datent de la fin du XIIIe début du XIVe siècle. Maintes fois remanié, le château présente aujourd'hui une architecture qui s'étage du XIVe au XVIIIe 
-    siècle. Il se dresse sur la commune de Gratot dans le département de la Manche en région Normandie.", 
-    "Bâti dès le XIIIe siècle, il est maintes fois transformé jusqu'à son apogée au XVIIIe siècle, avant d'être laissé à l'abandon au XIXe siècle, et de devenir aujourd'hui un lieu touristique.\n\n Le château a fait l'objet d'un classement
-     partiel au titre des Monuments historiques par arrêté du 4 août 1970. Seuls les ruines, les façades et toitures des communs avec la poterne d'entrée ainsi que les douves sont classés. Le château de Gratot est situé dans le département 
-     français de la Manche sur la commune de Gratot. ", 2);
+INSERT INTO ACTIVITE VALUES(3,'Château de Gratot','Gratot','Le château de Gratot est à l\'origine une maison forte, dont les vestiges subsistants datent de la fin du XIIIe début du XIVe siècle. Maintes fois remanié, le château présente aujourd\'hui une architecture qui s\'étage du XIVe au XVIIIe \n    siècle. Il se dresse sur la commune de Gratot dans le département de la Manche en région Normandie.','Bâti dès le XIIIe siècle, il est maintes fois transformé jusqu\'à son apogée au XVIIIe siècle, avant d\'être laissé à l\'abandon au XIXe siècle, et de devenir aujourd\'hui un lieu touristique.\n\n Le château a fait l\'objet d\'un classement\n     partiel au titre des Monuments historiques par arrêté du 4 août 1970. Seuls les ruines, les façades et toitures des communs avec la poterne d\'entrée ainsi que les douves sont classés. Le château de Gratot est situé dans le département \n     français de la Manche sur la commune de Gratot. ',2);
 
-INSERT INTO COMMENTAIRE VALUES(NULL, "Super lieu! Je recommande chaudement la traversée avec un guide!", 1, 1);
-INSERT INTO COMMENTAIRE VALUES(NULL, "Le cadre est magnifique!", 1, 2);
-INSERT INTO COMMENTAIRE VALUES(NULL, "Une visite très intéressant bien que courte.", 1, 2);
+INSERT INTO ACTIVITE VALUES(4,'Tapisserie de Bayeux','Bayeux (Centre Guillaume le Conquérant)','La tapisserie de Bayeux est une oeuvre d\'art médiévale, brodée à la main, et représentant la conquête de l\'Angleterre par le duc Normand et Roi d\'Angleterre Guillaume le conquérant.','La tapisserie de Bayeux, aussi connue sous le nom de \"tapisserie de la reine Mathilde\", et plus anciennement \"Telle du Conquest\", pour \"toile de la Conquête\", est une broderie (anciennement \"tapisserie aux points d\'aiguille\") du XIe siècle inscrite depuis 2007 au registre Mémoire du monde par l\'UNESCO1.\r\n\r\nElle décrit des faits allant de la fin du règne du roi d\'Angleterre Édouard le Confesseur en 1064 à la bataille d\'Hastings en 1066, dont l\'enjeu était le trône d\'Angleterre, contesté à Harold Godwinson par Guillaume, duc de Normandie. Les péripéties clés de la bataille, dont l\'issue détermina la conquête normande de l\'Angleterre, y sont détaillées, mais près de la moitié des scènes relatent des épisodes antérieurs à l\'invasion elle-même. Elle semble avoir été commandée par Odon de Bayeux, le demi-frère de Guillaume et réalisée au cours des années qui ont suivi la conquête.\r\n\r\nBien que présentant les événements sous un jour très favorable à Guillaume le Conquérant, au point d\'être considérée parfois comme une œuvre de propagande, elle a une valeur documentaire inestimable pour la connaissance du XIe siècle normand et anglais. Elle renseigne sur les vêtements, les châteaux, les navires et les conditions de vie de cette époque. De façon générale, elle constitue un des rares exemples de l\'art roman profane.\r\n\r\nConservée jusqu\'à la fin du XVIIIe siècle dans le Trésor de la cathédrale de Bayeux, elle échappa de peu à la destruction lors de la Révolution française. Elle est aujourd\'hui présentée au public au centre Guillaume le Conquérant qui lui est entièrement dédié.',3);
+
+INSERT INTO ACTIVITE VALUES(5,'Chateau de Caen','Caen (Centre-Ville)','Le Château de Caen est un château fort médiéval ayant abrité Guillaume le Conquérant au XIe siècle.','Le château de Caen est un château fort, fondé vers 1060 par Guillaume le Bâtard, remanié au fil des siècles, qui se dresse sur la commune française de Caen dans le département du Calvados, en région Normandie. Avec ses 5,5 hectares, c’est l’un des plus grands châteaux d’Europe.\r\n\r\nLe château est classé partiellement au titre des monuments historiques par arrêté du 10 avril 1972.\r\n\r\nIl accueille aujourd\'hui en son sein le musée des Beaux-Arts de Caen, et le musée de Normandie.',3);
+
+INSERT INTO COMMENTAIRE VALUES(1,'Super lieu! Je recommande chaudement la traversée avec un guide!',1,1);
+INSERT INTO COMMENTAIRE VALUES(2,'Le cadre est magnifique!',1,2);
+INSERT INTO COMMENTAIRE VALUES(3,'Une visite très intéressant bien que courte.',1,2),
+INSERT INTO COMMENTAIRE VALUES(4,'Superbe lieu plein de choses à faire, spacieux, bien conservé !',5,4);
+INSERT INTO COMMENTAIRE VALUES(5,'Une visite très plaisante pour compléter une journée au mont Saint-Michel. Je recommande !',2,4);
+INSERT INTO COMMENTAIRE VALUES(6,'La salicorne de la baie est délicieuse !',1,4);
+INSERT INTO COMMENTAIRE VALUES(7,'Incontournable dans la région.',4,4);
+INSERT INTO COMMENTAIRE VALUES(8,'Probablement la plus grande merveille du monde !',4,5);
+INSERT INTO COMMENTAIRE VALUES(9,'Joli, mais pas autant que la tapisserie de Bayeux...',3,5);
+
+
+INSERT INTO PHOTO VALUES(1,'20201205124704.jpeg',4);
+INSERT INTO PHOTO VALUES(2,'20201205124923.png',4);
+INSERT INTO PHOTO VALUES(3,'20201205125347.jpeg',4);
+INSERT INTO PHOTO VALUES(4,'20201205125711.jpeg',1)
+INSERT INTO PHOTO VALUES(5,'20201205125902.jpeg',1);
+INSERT INTO PHOTO VALUES(6,'20201205010014.jpeg',1);
+INSERT INTO PHOTO VALUES(7,'20201205010419.jpeg',2);
+INSERT INTO PHOTO VALUES(8,'20201205010450.jpeg',2);
+INSERT INTO PHOTO VALUES(9,'20201205010711.jpeg',2);
+INSERT INTO PHOTO VALUES(10,'20201205011949.jpeg',3);
+INSERT INTO PHOTO VALUES(11,'20201205012018.png',3);
+INSERT INTO PHOTO VALUES(12,'20201205012230.jpeg',3);
+INSERT INTO PHOTO VALUES(13,'20201205013546.jpeg',5);
+INSERT INTO PHOTO VALUES(14,'20201205013702.jpeg',5);
+INSERT INTO PHOTO VALUES(15,'20201205013748.jpeg',5);
